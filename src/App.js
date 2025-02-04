7a1070b2123f7e6d455e550927a04aa86383d731
Harish H J <harishjayram95@gmail.com>
2025-02-04 12:59:36 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   // let ele  = React.createElement("div" , {className:"App"} , 
+//   //   React.createElement("h1" ,null, "Header")
+//   // )
+//   let username = "Maheshwari";
+//   return (
+//     // <div className="App">
+//     //  <h1>Header</h1>
+//     //  <input />
+//     // </div>
+//     // ele
+//   //  <section>
+//   //    <h1>One JSx element</h1>
+//   //   <p>lorm3
+//   //   </p>
+//   //  </section>
+// <div>
+// <h1>{username}</h1>
+//   <p>{100+100}</p>
+// </div>
+//   );
+// }
+// export default App;
+
+// ! components
+
+// !CBC
+// import React from "react";
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <h1>Class based component</h1>
+//     )
+//   }
+// }
+// export default App;
+
+// ! FBC
+
+// function App(){
+//   return(
+//     <h1>Function based component</h1>
+//   )
+// }
+// export default App;
+
+//! OR
+
+// const App=()=>{
+//   return(
+//     <h1>Arrow function component</h1>
+//   )
+// }
+// export default App;
+
+// ! component composition
+// import React from 'react'
+// import Navbar from './components/Navbar'
+// import Main from './components/Main'
+// import Sidebar1 from './components/Sidebar1'
+// import Sidebar2 from './components/Sidebar2'
+// import Footer from './components/Footer'
+// const App = () => {
+//   return (
+//     <div className='app'>
+//       <Navbar/>
+//       <Main/>
+//       <div className='sidebar'>
+//       <Sidebar1/>
+//       <Sidebar2/>
+//       </div>
+//       <Footer/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// !props
+import React, { Component } from 'react'
+import CBCPropEx from './propsex/CBCPropEx'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <CBCPropEx username="Vamshi"/>
+      </div>
+    )
+  }
 }
-
-export default App;
